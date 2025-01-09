ecbf9f05ab1e4016f76d495008604701a9cab042
frank <frank@in-havok.com>
2025-01-09 11:45:21 -0500

    Add types file and tsconfig

---
+++ test-types.ts
@@ -0,0 +1,13 @@
+// Types
+let id: number = 5;
+let company: string = "Ford";
+let isTrue: boolean = false;
+
+// Collections
+let ids: number[] = [2,3,4,5];
+
+
+// Any
+let x: any;
+let y: any = "some string";
+let xArr: any[] = [1, "here", false];
