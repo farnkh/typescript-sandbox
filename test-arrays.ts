ca47dddbbdcdb116c653b1142c7d3d6fb4935a2d
frank <frank@in-havok.com>
2025-01-10 09:16:05 -0500

    Added array test file

---
+++ test-arrays.ts
@@ -0,0 +1,21 @@
+// Arrays
+let names: string[] = ["john", "mike", "frank"];
+let ages: number[] = [100, 12, 33];
+
+// Alt Syntax
+let altNames: Array<string> = ["john", "mike", "frank"];
+let altAges: Array<number> = [100, 12, 33];
+
+//
+// Multuiidimensional
+const board: string[][] = [
+    ["X","O", "X"], 
+    ["X","O", "X"], 
+    ["X","O", "X"], 
+]
+
+const threeDimBoard: number[][][] = [
+    [
+        [4]
+    ]
+]
