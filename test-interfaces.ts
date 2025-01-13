621972d3328ecc7a2674878cde4cca3596f77924
frank <frank@in-havok.com>
2025-01-13 11:21:17 -0500

    Adding test interface file and updating gitignore

--- test-interfaces.ts
+++ test-interfaces.ts
@@ -1,5 +1,6 @@
 //
-// Interfaces
+// Interfaces (describe objects)
+// NO EQUAL SIGN LIKE TYPES
 interface Animal {
     name: string,
     age: number,
@@ -10,3 +11,42 @@ interface Animal {
 const dog:Animal = {name: "buddy", age: 11};
    dog.age = 12
 
+interface PointXY {
+    readonly id: number,
+    x: number,
+    y: number,
+    z?: number,
+}
+
+let p: PointXY = {id:1, x:100, y:100}
+
+interface Animal_ {
+    name: string,
+    age: number,
+    type?: string,
+    speak: ()=> string;
+    // alternative way speak(): string,
+};
+
+const doggy:Animal_ = {
+    name: "fido",
+    age: 3,
+    speak: ()=> "bark bark",
+}
+
+console.log(doggy.speak());
+
+
+interface Product {
+    name: string,
+    price: number,
+    applyDiscount(discount:number): number,
+}
+
+const shoes: Product ={
+    name: "red shoes",
+    price: 100,
+    applyDiscount(amount: number){
+        return this.price * amount;
+    }
+}
